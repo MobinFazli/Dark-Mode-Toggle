4737bf498e1376e14bad67f25f7b518e2e322768
Mobin Fazli <m0binfae2003@gmail.com>
2024-02-14 15:45:20 -0500

    Dark Mode Toggle

--- src.js
+++ src.js
@@ -1,16 +1,26 @@
+// Selects the toggle switch element with the class toggle!
 const toggleEl = document.querySelector(".toggle");
 
-const bodyEl   = document.querySelector("body");
+// Selects the body element!
+const bodyEl = document.querySelector("body");
 
+// Sets the checked property of the toggle switch based on the value retrieved from localStorage!
 toggleEl.checked = JSON.parse(localStorage.getItem("theme"));
 
-updateBody();
+// Calls the updateBodyMode function to initially set the background color based on the toggle state!
+updateBodyMode();
 
-function updateBody() {
+// Defines a function to update the body background color based on the toggle state!
+function updateBodyMode() {
+    // Sets the background color of the body to black if the toggle is checked, otherwise white!
     bodyEl.style.background = toggleEl.checked ? "black" : "white";
 }
 
+// Adds an event listener to the toggle switch for input (change) events!
 toggleEl.addEventListener("input", () => {
-    updateBody();
+     // Calls the updateBody function to update the body background color when the toggle state changes!
+    updateBodyMode();
+
+     // Updates the localStorage with the current toggle state!
     localStorage.setItem("theme", JSON.stringify(toggleEl.checked));
 });
